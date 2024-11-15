e7b10e386c10559628e33aae684311988cd1fe8a
yuanzhihong.chiwei <yuanzhihong.chiwei@bytedance.com>
2024-11-16 03:17:00 +0800

    card v2 use body

--- src/v2/card_v2.ts
+++ src/v2/card_v2.ts
@@ -2,24 +2,16 @@ import { CardHeader } from "../title";
 import { Config } from "./config";
 import { CardElementV2 } from "./element";
 
-export class LarkV2Card {
-  schema: "2.0";
-  config: Config;
-  header: CardHeader;
+class Body {
   elements: CardElementV2[];
   elementIdMap: Map<string, CardElementV2>;
-
-  constructor(header: CardHeader, config: Config) {
-    this.schema = "2.0";
-    this.header = header;
+  constructor() {
     this.elements = [];
-    this.config = config;
     this.elementIdMap = new Map();
   }
 
   addElements(...elements: CardElementV2[]): this {
     this.elements.push(...elements);
-
     for (const element of elements) {
       if (element.element_id) {
         // 检查是否已经存在相同的 element_id
@@ -31,16 +23,30 @@ export class LarkV2Card {
         throw new Error("Element must have an element_id");
       }
     }
-
     return this;
   }
-
   toJSON(): any {
     return {
-      schema: this.schema,
-      header: this.header,
-      elements: this.elements.map((element) => element),
-      config: this.config,
+      elements: this.elements,
     };
+  }
+}
+
+export class LarkV2Card {
+  schema: "2.0";
+  config: Config;
+  header: CardHeader;
+  body: Body;
+
+  constructor(header: CardHeader, config: Config) {
+    this.schema = "2.0";
+    this.header = header;
+    this.config = config;
+    this.body = new Body();
+  }
+
+  addElements(...elements: CardElementV2[]): this {
+    this.body.addElements(...elements);
+    return this;
   }
 }
